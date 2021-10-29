51f387d80032bf8bc8a7a50fa3d353893ee279e3
andrija-s <51809572+andrija-s@users.noreply.github.com>
2021-10-28 21:58:58 -0700

    fixing this thing once an for all

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -131,7 +131,7 @@ class RecipeCard extends HTMLElement {
        inner_s1.innerText = rating_val;
        div_rating.appendChild(inner_s1);
        const inner_img = document.createElement('img');
-       inner_img.setAttribute('src', `https://github.com/andrija-s/Lab6_Starter/blob/main/assets/images/icons/${num_rating}-star.svg`);
+       inner_img.setAttribute('src', `https://raw.githubusercontent.com/andrija-s/Lab6_Starter/main/assets/images/icons/${num_rating}-star.svg`);
        inner_img.setAttribute('alt', `${num_rating} stars`);
        div_rating.appendChild(inner_img);
        const inner_s2 = document.createElement('span');
